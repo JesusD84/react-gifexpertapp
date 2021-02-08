04cff5bdb2c266853851e834639c82ad6f7435d1
JesusD <chuy.delgado.hernandez@gmail.com>
2021-02-08 17:15:00 -0600

    Probando GrifGridItem

---
+++ tests/components/GifGridItem.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGridItem } from '../../src/components/GifGridItem';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+describe('Pruebas en <GifGridItem />', () => {
+    
+    test('debe de mostrar el componente correctamente', () => {
+
+        const {data: images, loading} = useFetchGifs(category);
+        
+        const wrapper = shallow(<GifGridItem 
+            key={images[0].id}
+            title={images[0].title}
+            url={images[0].url}
+        />);
+
+        expect(wrapper).toMatchSnapshot();
+        
+    });
+    
+
+})
